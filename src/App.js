81ca93bb9d3233a0f66adefac04083f26a06c9ca
Subhankhalid1 <rsubhankhalid@gmail.com>
2021-04-02 01:11:39 +0500

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react'
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+
+useEffect(() => {
+//   async function getGlobalData() {
+ 
+//     const response = await fetch('/api/user');
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+    
+// }
+// getGlobalData();
+
+
+
+axios.get('/api/user')
+.then(response=>{
+  console.log(response.data)
+})
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h2>Hello World from MERN STACK </h2>
     </div>
   );
 }
